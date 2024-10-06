90b5117cd9cb6eacce34acf75c6621629a1de8ae
Zaitsev-ap <54988721+Zaitsev-ap@users.noreply.github.com>
2024-10-06 22:18:02 +0300

    fix

--- scripts/cycle_execs.php
+++ scripts/cycle_execs.php
@@ -14,7 +14,7 @@ include_once(DIR_MODULES . "control_modules/control_modules.class.php");
 $ctl = new control_modules();
 $checked_time = 0;
 setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
-$cycleVarName='ThisComputer.'.str_replace('.php', '', basename(__FILE__)).'Run';
+//$cycleVarName='ThisComputer.'.str_replace('.php', '', basename(__FILE__)).'Run';
 $cycleVarNameRUN=str_replace('.php', '', basename(__FILE__)) . "Run";
 
 echo date("H:i:s") . " running " . basename(__FILE__) . "\n";
